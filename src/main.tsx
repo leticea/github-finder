a2970aa978d6a59ded86ea6c4df5b72f8bd1768e
leticea <nascimento.leticeah@gmail.com>
2023-01-14 15:59:02 -0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from "./routes/Home";
+
+const router = createBrowserRouter([
+  {
+    
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
