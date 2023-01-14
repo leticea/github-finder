213f3f86e34a8f1e2e948bb0835c2b36ef655249
leticea <nascimento.leticeah@gmail.com>
2023-01-14 16:51:46 -0300

    criando dados do user

---
+++ src/components/User.tsx
@@ -0,0 +1,27 @@
+import { UserProps } from '../types/user';
+import { MdLocationPin } from 'react-icons/md';
+
+const User = ({ login, avatar_url, location, followers, following }: UserProps) => {
+  return (
+    <div>
+      <img src={avatar_url} alt={login} />
+      <h2>{login}</h2>
+      <p>
+        <MdLocationPin />
+        <span>{location}</span>
+      </p>
+      <div>
+        <div>
+          <p>Seguidores:</p>
+          <p>{followers}</p>
+        </div>
+        <div>
+          <p>Seguindo:</p>
+          <p>{following}</p>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default User;
