c0239bfc1c91222bb8ac69da720aba312efcf47b
leticea <nascimento.leticeah@gmail.com>
2023-01-14 16:15:37 -0300

    tipando dados com ts

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,10 @@
+import { UserProps } from '../types/user';
 import Search from '../components/Search';
+import { useState } from 'react';
 
 const Home = () => {
+  const [user, setUser] = useState<UserProps | null>(null);
+
   return (
     <div>
       <Search />
